a93b3dd524dc97e2811f85db594b352c1374a7e5
rrahadis <rahadi.solehudin@gmail.com>
2020-08-25 15:37:48 +0700

    catatan perubahan 2

--- service-worker.js
+++ service-worker.js
@@ -5,38 +5,19 @@ if (workbox)
 else
   console.log(`Workbox gagal dimuat`);
   
-workbox.precaching.precacheAndRoute([
-    { url: '/index.html', revision: '1' },
-    { url: '/nav.html', revision: '1' },
-    { url: '/detail.html', revision: '1' },
-    { url: '/css/materialize.min.css', revision: '1' },
-    { url: '/css/materialize.css', revision: '1' },
-    { url: '/css/style.css', revision: '1' },
-    { url: '/js/materialize.min.js', revision: '1' },
-    { url: '/js/materialize.js', revision: '1' },
-    { url: '/js/script.js', revision: '1' },
-    { url: '/js/nav.js', revision: '1' },
-    { url: '/js/api.js', revision: '1' },
-    { url: '/js/db.js', revision: '1' },
-    { url: '/js/idb.js', revision: '1' },
-    { url: '/push.js', revision: '1' },
-    { url: '/pages/team.html', revision: '1' },
-    { url: '/pages/league.html', revision: '1' },
-    { url: '/pages/saved.html', revision: '1' },
-    { url: '/manifest.json', revision: '1' },
-    { url: '/package-lock.json', revision: '1' },
-    { url: '/img/bola.png', revision: '1' },
-    { url: '/img/img512.png', revision: '1' },
-    { url: '/img/logo192.png', revision: '1' },
-],{
-    ignoreUrlParametersMatching: [/.*/]
-});
+workbox.routing.registerRoute(
+  ({url}) => url.origin,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'cache-data'  
+  })
+);
+
 
 workbox.routing.registerRoute(
-  new RegExp('https://api.football-data.org/v2/'),
-    workbox.strategies.staleWhileRevalidate({
-        cacheName: 'football-data'
-    })
+  ({url}) => url.origin === 'https://api.football-data.org/v2/',
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'football-data'
+  })
 );
 
 self.addEventListener("push", function (event) {
